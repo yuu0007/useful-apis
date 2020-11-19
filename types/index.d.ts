b8bf25082d103edecf354b839383723bb20d6ce4
yuu0007 <jmquimosing@icloud.com>
2020-11-19 01:53:59 +0000

    added types

---
+++ types/index.d.ts
@@ -0,0 +1,23 @@
+export function duncte(...args: any[]): any;
+
+export function someRandomApi(...args: any[]): any;
+
+export namespace duncte {
+    function decodeToken(...args: any[]): void;
+
+    function getJoke(...args: any[]): void;
+
+    function getMeme(...args: any[]): void;
+
+    function love(...args: any[]): void;
+
+    function npm(...args: any[]): void;
+
+    function randomString(...args: any[]): void;
+
+}
+
+export namespace someRandomApi {
+    function chat(...args: any[]): void;
+
+}
