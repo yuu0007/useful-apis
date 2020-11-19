2423e7ac5d60e9f0cc85c5e81c2a0fa25aee0fc5
yuu0007 <jmquimosing@icloud.com>
2020-11-19 05:45:08 +0000

    added postinstall script

---
+++ install/postinstall.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk');
+
+setTimeout(() => {
+	console.log(chalk.green(`#    #  ####  ###### ###### #    # #              ##   #####  #  ####  
+#    # #      #      #      #    # #             #  #  #    # # #      
+#    #  ####  #####  #####  #    # #     ###### #    # #    # #  ####  
+#    #      # #      #      #    # #            ###### #####  #      # 
+#    # #    # #      #      #    # #            #    # #      # #    # 
+ ####   ####  ###### #       ####  ######       #    # #      #  ####`));
+}, 1000);
+setTimeout(() => console.log(chalk.blue('Thanks for installing useful-apis!')), 1000);
+setTimeout(() => console.log(chalk.blue('Github repo: https://github.com/yuu0007/useful-apis')), 1000);
+setTimeout(() => console.log(chalk.blue('Documentation: https://useful-apis.js.org')), 1000);
